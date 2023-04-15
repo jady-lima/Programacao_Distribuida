884b9fb565b4ee40830768f10a4c6d8255c889c7
Jady Lima <jadylsilva@gmail.com>
2023-04-15 17:57:10 -0300

    Create Client.java

---
+++ HelloWorld/Client.java
@@ -0,0 +1,24 @@
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+
+public class Client {
+
+    public static void main(String args[]) {
+        try {
+            // Obtenha uma referência ao registro RMI
+            Registry registry = LocateRegistry.getRegistry();
+
+            // Obtenha a referência ao objeto remoto
+            Hello stub = (Hello) registry.lookup("Hello");
+
+            // Chame o método remoto
+            String response = stub.sayHello();
+
+            System.out.println("Resposta do servidor: " + response);
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
