5673a26e8197c474052576e2228af704022fad76
Jady Lima <jadylsilva@gmail.com>
2023-04-15 17:57:19 -0300

    Create HelloImpl.java

---
+++ HelloWorld/HelloImpl.java
@@ -0,0 +1,13 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class HelloImpl extends UnicastRemoteObject implements Hello {
+
+    public HelloImpl() throws RemoteException {
+        super();
+    }
+
+    public String sayHello() throws RemoteException {
+        return "Hello, world!";
+    }
+}
