b82290cb203d702e416dddb7744e9f4b378f6e3b
Jady Lima <jadylsilva@gmail.com>
2023-04-20 20:45:07 -0300

    Create TemperatureConverterServer.java

---
+++ TemperatureConverter/TemperatureConverterServer.java
@@ -0,0 +1,23 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
+public class TemperatureConverterServer 
+{
+    public static void main(String[] args) {
+        try {
+            LocateRegistry.createRegistry(1029);
+            TemperatureConverter temperatureConverter = new TemperatureConverterImp();
+            Naming.rebind("rmi://localhost:1029/TemperatureConverter", temperatureConverter);
+            System.out.println("Server is ready");
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } 
+        catch (MalformedURLException e) {
+            System.out.println("Erro");
+            e.printStackTrace();
+        }
+    }
+}
