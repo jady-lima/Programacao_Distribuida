917238315ab685c0ccf4c1c897b470b53d69fcaf
Jady Lima <jadylsilva@gmail.com>
2023-04-20 20:44:41 -0300

    Create TemperatureConverter.java

---
+++ TemperatureConverter/TemperatureConverter.java
@@ -0,0 +1,12 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface TemperatureConverter extends Remote
+{
+    double CelsiusParaFahrenheit(double celsius) throws RemoteException;
+    double CelsiusParaKelvin(double celsius) throws RemoteException;
+    double FahrenheitParaCelsius(double fahrenheit) throws RemoteException;
+    double FahrenheitParaKelvin(double fahrenheit) throws RemoteException;
+    double KelvinParaCelsius(double kelvin) throws RemoteException;
+    double KelvinParaFahrenheit(double kelvin) throws RemoteException;
+}
